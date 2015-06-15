678bd72e789c5cc077dd21481f37fc2de5bc05ca
Branden <boden@aii.edu>
2015-06-14 22:05:13 -0700

    updated plane settings

--- Plane.cpp
+++ Plane.cpp
@@ -2,7 +2,7 @@
 
 Plane::Plane(GLuint width, GLuint height) : Object(){
 	//BuildTriangleStrip(this->width = width, this->height = height, false);
-	BuildTriangles(this->width = width, this->height = height, false);
+	BuildTriangles(this->width = width, this->height = height, true);
 }
 
 Plane::~Plane(){
